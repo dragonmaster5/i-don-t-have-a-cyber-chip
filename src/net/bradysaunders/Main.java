6438a636d829e6ee63bea08cc764a48c01aac2d4
dragonmaster5 <griffindorv@gmail.com>
2018-06-26 15:17:54 -0400

    palindrome checker

--- src/net/bradysaunders/Main.java
+++ src/net/bradysaunders/Main.java
@@ -1,7 +1,16 @@
 package net.bradysaunders;
-
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
-	 System.out.println("hi");    }
-}
+        public static boolean main(String[] args) {
+            private static  boolean isPalindrome;(String input; input) {
+           int half = (int) Math.floor(input.lenght() / 2D ) ;
+            for(int i=0; i < half; ++i)}
+            Scanner scanner = new Scanner (System.in);
+     String palindrome = scanner.nextLine();
+     if(isPalindrome(palindrome)) {
+         System.out.println("that is a palidrome");
+     }
+     else {
+         System.out.println (" this is not a palindrome")
+
